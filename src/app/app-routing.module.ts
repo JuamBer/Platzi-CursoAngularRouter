82c60efe7651ce35d499efb0f8ef42339abf19e2
Juan Sáez García <juambersaezgarcia@gmail.com>
2022-05-14 19:59:26 +0200

    Creando en Website Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,59 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { HomeComponent } from './website/pages/home/home.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { MyCartComponent } from './website/pages/mycart/mycart.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent
-      },
-      {
-        path: 'my-cart',
-        component: MyCartComponent,
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-      },
-      {
-        path: 'recovery',
-        component: RecoveryComponent,
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-      },
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-      },
-    ]
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
   },
   {
     path: 'cms',
