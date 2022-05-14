d9877dcf2856f8b02dbb14e3cccceda182a935c1
Juan Sáez García <juambersaezgarcia@gmail.com>
2022-05-14 20:10:09 +0200

    Creando un Shared Module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+import { ImgComponent } from './components/img/img.component';
+import { SwiperModule } from 'swiper/angular';
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductsComponent,
+    ImgComponent,
+
+    ReversePipe,
+    TimeAgoPipe,
+
+    HighlightDirective,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SwiperModule
+  ],
+  exports: [
+    ProductComponent,
+    ProductsComponent,
+    ImgComponent,
+
+    ReversePipe,
+    TimeAgoPipe,
+
+    HighlightDirective,
+  ]
+})
+export class SharedModule { }
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,13 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { WebsiteRoutingModule } from './website-routing.module';
 import { SwiperModule } from 'swiper/angular';
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
+
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/mycart/mycart.component';
@@ -19,16 +14,11 @@ import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
     CategoryComponent,
     MyCartComponent,
@@ -42,7 +32,8 @@ import { LayoutComponent } from './components/layout/layout.component';
   imports: [
     CommonModule,
     SwiperModule,
-    WebsiteRoutingModule
+    WebsiteRoutingModule,
+    SharedModule
   ]
 })
 export class WebsiteModule { }
