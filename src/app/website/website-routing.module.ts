62e5fce89a56ae4477073e3885bfe5c8edc28cc9
Juan Sáez García <juambersaezgarcia@gmail.com>
2022-05-14 21:15:22 +0200

    AllModules y CustomStrategy

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -21,7 +21,10 @@ const routes: Routes = [
       },
       {
         path: 'category',
-        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule)
+        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule),
+        data: {
+          preload: true
+        }
       },
       {
         path: 'product/:id',
